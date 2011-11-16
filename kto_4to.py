35ffed5d06678e5cdcea8a503d9e8d579412f909
Arseniy Kaploun <kapliars@gmail.com>
2011-11-17 00:26:14 +0400

    add total hours, by default all logs are shown
    - add total hours by dev
    - if release-date argument is not specified, display all logs for the version

--- kto_4to.py
+++ kto_4to.py
@@ -21,42 +21,33 @@ auth, client, project_name, to_datetime = (jira.auth, jira.client, jira.project_
 
 version = sys.argv[1]
 
+release_date = None
 if len(sys.argv) > 2:
     release_date = Datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')
-else:
-    versions = client.service.getVersions(auth, jira.project_name)
-    release_date = None
-    for v in versions:
-        if v.name == version:
-            release_date = to_datetime(v.releaseDate)
-            break
-    if not release_date:
-        print "Can not find version", version
-        exit(1)
 
 print "Logs for version %s, after %s" % (version, release_date)
 
 issues = jira.client.service.getIssuesFromJqlSearch(auth, 'project = %s and fixVersion = %s' % (jira.project_name, version),
                                                     1000)
 
-stats = {} # user -> [date, issue, time]
+logs = {} # user -> [date, issue, time]
+stats = {} # user -> total hours
 for issue in issues:
     worklogs = client.service.getWorklogs(auth, issue.key)
     for l in worklogs:
-        if l.author not in stats:
-            stats[l.author] = []
+        if l.author not in logs:
+            logs[l.author] = []
         wl_time = to_datetime(l.created)
         if not release_date or wl_time >= release_date:
-#            print "Issue: ", issue.key
-#            print "Worklog: ", l
-        #if wl_time <= release_date or len(sys.argv) < 3:
-        #    stats[l.author] += l.timeSpentInSeconds
-            stats[l.author].append( (wl_time, issue.key, l.timeSpentInSeconds / 3600.0) )
+            if l.author not in stats:
+                stats[l.author] = 0
+            stats[l.author] += l.timeSpentInSeconds / 3600.0
+            logs[l.author].append( (wl_time, issue.key, l.timeSpentInSeconds / 3600.0) )
 
 print "Work logged by engineer\ndev\t\tstarted\t\tissue\t\thours"
 
-for author in stats:
-    print author
-    stats[author].sort()
-    for entry in stats[author]:
+for author in logs:
+    print "%s\t\t\t\t\t\t%s hours" % (author, stats[author])
+    logs[author].sort()
+    for entry in logs[author]:
         print "\t\t%s\t\t%s\t\t%s" % entry
