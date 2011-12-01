394eb322bf2af2f4a76ceffed77abe28009b4c0f
Andrew Klochkov <aklochkov@griddynamics.com>
2011-11-30 18:22:03 -0800

    Remove unnecessary dependencies

--- setup.py
+++ setup.py
@@ -13,10 +13,6 @@ setup(
     packages=find_packages(),
     namespace_packages=['jirareports'],
     install_requires=[
-        'MySQL-python == 1.2.3',
-        'lockfile == 0.8',
-        'python-daemon == 1.5.5',
-        'sqlalchemy == 0.7.3',
         'SOAPpy'
     ]
 )
